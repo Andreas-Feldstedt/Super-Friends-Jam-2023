5bc8e8e7df22c7339ad49ac757c152ca41202c90
Andreas Feldstedt <afeldstedt@gmail.com>
2023-07-30 11:53:04 +0200

    Implemented audio looping

--- BeatMachine/BeatMachine.cs
+++ BeatMachine/BeatMachine.cs
@@ -6,111 +6,116 @@ namespace Proto;
 
 public partial class BeatMachine : AudioStreamPlayer2D
 {
-	#region Singleton
-	private static BeatMachine _instance;
-	public static BeatMachine Instance => _instance;
-	#endregion Singleton
+    #region Singleton
+    private static BeatMachine _instance;
+    public static BeatMachine Instance => _instance;
+    #endregion Singleton
 
-	private AudioStreamGeneratorPlayback _playback;
+    private AudioStreamGeneratorPlayback _playback;
 
-	/// beats per minute
-	[Export] public float Bpm = 100;
-	
-	/// seconds per beat 
-	[Export] private float _spb;
-	
-	[ExportCategory("Debug")]
-	[Export] private float _songPosSecs;
-	[Export] private float _songPosBeat;
-	
-	/// <summary>
-	/// We greedily fill the buffer so that there is always something to play
-	/// </summary>
-	/// <remarks>
-	/// This is NOT the current position of the song for gameplay or other reasons
-	/// </remarks>
-	[Export] private int _prefilledBeat;
-	
-	[ExportCategory("Gameplay Debug")]
-	[Export] private int _gameplayBeat;
-	
-	#region Beat Processors
-	
-	private readonly List<IProcessBeat> _beatProcessors = new();
-	
-	private string[] _beatInputs =
-	{
-		"TestBeatActoin",
-		"up",
-		"down",
-		"left",
-		"right"
-	};
-		
-	private Godot.Collections.Dictionary<string, float> _inputAnticipation;
-	
-	#endregion Beat Processors
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Debug.Assert(_instance == null);
-		_instance = this;
-		
-		_spb = 60f / Bpm;
-	
-		_inputAnticipation = new();
-		foreach (var key in _beatInputs)
-			_inputAnticipation[key] = -1;
+    /// beats per minute
+    [Export] public float Bpm = 100;
 
-	}
-	
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		_songPosSecs = GetPlaybackPosition()
-					   + (float)AudioServer.GetTimeSinceLastMix()
-					   - (float)AudioServer.GetOutputLatency();
-	
-		_songPosBeat = _songPosSecs / _spb;
-		
-		foreach (var beatInput in _beatInputs)
-			if (Input.IsActionJustPressed(beatInput))
-			{
-				_inputAnticipation[beatInput] = Mathf.PosMod(_songPosBeat, 1f);
-			}
+    /// seconds per beat 
+    [Export] private float _spb;
 
-		if (Mathf.FloorToInt(_songPosBeat) > _gameplayBeat)
-		{
-			// do gameplay stuff
-			++_gameplayBeat;
-			for (var i = 0; i < _beatProcessors.Count; i++)
-			{
-				var beatProcessor = _beatProcessors[i];
-				if (!IsInstanceValid(beatProcessor as GodotObject))
-				{
-					_beatProcessors.RemoveAt(i);
-					--i;
-				}
-				beatProcessor.ProcessBeat(_gameplayBeat);
-			}
+    [ExportCategory("Debug")]
+    [Export] private float _songPosSecs;
+    [Export] private float _songPosBeat;
 
-			foreach (var key in _inputAnticipation.Keys)
-				_inputAnticipation[key] = -1;
-		}
-	}
+    /// <summary>
+    /// We greedily fill the buffer so that there is always something to play
+    /// </summary>
+    /// <remarks>
+    /// This is NOT the current position of the song for gameplay or other reasons
+    /// </remarks>
+    [Export] private int _prefilledBeat;
 
-	#region Singleton Interface
+    [ExportCategory("Gameplay Debug")]
+    [Export] private int _gameplayBeat;
 
-	public int SingletonBeat => _gameplayBeat;
-	public float SingletonTimeBeat => _songPosBeat;
-	public float SingletonTimeSecs => _songPosSecs;
+    #region Beat Processors
 
-	public void SingletonRegisterBeatProcessor(IProcessBeat processor) { _beatProcessors.Add(processor); }
-	public void SingletonUnregisterBeatProcessor(IProcessBeat processor) { _beatProcessors.Remove(processor); }
-	
-	public bool SingletonIsActionPressedBeat(string action) => _inputAnticipation[action] > 0;
-	public float SingletonGetActionAnticipation(string action) => _inputAnticipation[action];
-	
-	#endregion Singleton Interface
+    private readonly List<IProcessBeat> _beatProcessors = new();
+
+    private string[] _beatInputs =
+    {
+        "TestBeatActoin",
+        "up",
+        "down",
+        "left",
+        "right"
+    };
+
+    private Godot.Collections.Dictionary<string, float> _inputAnticipation;
+
+    #endregion Beat Processors
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Debug.Assert(_instance == null);
+        _instance = this;
+
+        _spb = 60f / Bpm;
+
+        _inputAnticipation = new();
+        foreach (var key in _beatInputs)
+            _inputAnticipation[key] = -1;
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        _songPosSecs = GetPlaybackPosition()
+                       + (float)AudioServer.GetTimeSinceLastMix()
+                       - (float)AudioServer.GetOutputLatency();
+
+        _songPosBeat = _songPosSecs / _spb;
+
+        foreach (var beatInput in _beatInputs)
+            if (Input.IsActionJustPressed(beatInput))
+            {
+                _inputAnticipation[beatInput] = Mathf.PosMod(_songPosBeat, 1f);
+            }
+
+        if (Mathf.FloorToInt(_songPosBeat) > _gameplayBeat)
+        {
+            // do gameplay stuff
+            ++_gameplayBeat;
+            for (var i = 0; i < _beatProcessors.Count; i++)
+            {
+                var beatProcessor = _beatProcessors[i];
+                if (!IsInstanceValid(beatProcessor as GodotObject))
+                {
+                    _beatProcessors.RemoveAt(i);
+                    --i;
+                }
+                beatProcessor.ProcessBeat(_gameplayBeat);
+            }
+
+            foreach (var key in _inputAnticipation.Keys)
+                _inputAnticipation[key] = -1;
+        }
+
+        if (_gameplayBeat > 0 && Mathf.FloorToInt(_songPosBeat) == 1)
+        {
+            _gameplayBeat = 1;
+        }
+    }
+
+    #region Singleton Interface
+
+    public int SingletonBeat => _gameplayBeat;
+    public float SingletonTimeBeat => _songPosBeat;
+    public float SingletonTimeSecs => _songPosSecs;
+
+    public void SingletonRegisterBeatProcessor(IProcessBeat processor) { _beatProcessors.Add(processor); }
+    public void SingletonUnregisterBeatProcessor(IProcessBeat processor) { _beatProcessors.Remove(processor); }
+
+    public bool SingletonIsActionPressedBeat(string action) => _inputAnticipation[action] > 0;
+    public float SingletonGetActionAnticipation(string action) => _inputAnticipation[action];
+
+    #endregion Singleton Interface
 }
